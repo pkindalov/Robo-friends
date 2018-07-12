4356287b53931d6d3eeaa3c41774575f81b2e7c3
pkindalov <panayotkindalov@gmail.com>
2018-07-12 11:22:16 +0300

    Added properties to component Card and updated it to use them.

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ id, username, name, email}) => {
+  
+  let url = `https://www.robohash.org/${id}?200x200`
+
   return (
     <div className='bg-light-green dib br3  pa3 ma2 grow bw2 shadow-5'>
-      <img src='https://www.robohash.org/mishoselskibek?200x200' alt='robots' />
+      <img src={url} alt={name} />
       <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@gmail.com</p>
+        <h2>{username}</h2>
+        <p>{email}</p>
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import Card from './Card'
 import 'tachyons'
 import registerServiceWorker from './registerServiceWorker'
 
-import robots from './robots'
+import  { robots } from './robots'
 
 ReactDOM.render(
   <div>
-    <Card />
-    <Card />
-    <Card />
+    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email} />
   </div>
 
     , document.getElementById('root'))
