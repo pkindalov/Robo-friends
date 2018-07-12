5ee6d68b9dacad044c4b98dbf0e457e3a5101efe
pkindalov <panayotkindalov@gmail.com>
2018-07-12 11:50:08 +0300

    Created new component CardList for listing all robocards

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Card from './Card'
+
+import CardList from './CardList'
 import 'tachyons'
 import registerServiceWorker from './registerServiceWorker'
 
-import  { robots } from './robots'
+import { robots } from './robots'
 
 ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email} />
-  </div>
+  <CardList robots={robots} />
 
     , document.getElementById('root'))
 registerServiceWorker()
