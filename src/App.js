48214da2f4ace1dd87b4078617fb9b58383a692f
pkindalov <panayotkindalov@gmail.com>
2018-07-12 20:13:40 +0300

    Updated component App. Now the information for the robtos is coming from the server, not from a local file.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import { robots } from './robots'
+// import { robots } from './robots'
 
 import './App.css'
 
@@ -9,9 +9,17 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      robots: robots,
+    //   robots: robots,
+      robots: [],
       searchfield: ' '
     }
+  }
+
+  componentDidMount (){
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {this.setState({ robots: users})})  
+
   }
 
   onSearchChange  = (event) => {
@@ -26,15 +34,21 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
           })
 
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+          if(this.state.robots.length === 0){
+              return <h1>Loading..</h1>
+          } else {
+              
+              return (
+                <div className='tc'>
+                  <h1 className='f1'>RoboFriends</h1>
+                  <SearchBox searchChange={this.onSearchChange} />
+                  <CardList robots={filteredRobots} />
+          
+                </div>
+          
+              )
+          }
 
-      </div>
-
-    )
   }
 }
 
