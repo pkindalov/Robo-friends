db7074891906478a5f388f64fc6ba4f31ae0683e
pkindalov <panayotkindalov@gmail.com>
2018-07-12 20:30:10 +0300

    Added new component Scroll, and use it to make CardList component to be scrollable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 // import { robots } from './robots'
+import Scroll from './Scroll'
 
 import './App.css'
 
@@ -42,7 +43,9 @@ class App extends Component {
                 <div className='tc'>
                   <h1 className='f1'>RoboFriends</h1>
                   <SearchBox searchChange={this.onSearchChange} />
-                  <CardList robots={filteredRobots} />
+                  <Scroll>
+                    <CardList robots={filteredRobots} />
+                  </Scroll>
           
                 </div>
           
