9bd55c5c39a764c7f6cce02c99e986a825bc999f
pkindalov <panayotkindalov@gmail.com>
2018-07-13 12:27:59 +0300

    Moved all files in folders structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 // import CardList from './CardList'
-import App from './App'
+import App from './containers/App'
 import 'tachyons'
 import registerServiceWorker from './registerServiceWorker'
 
